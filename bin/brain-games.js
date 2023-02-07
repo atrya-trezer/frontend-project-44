8736e7378ac7340dbea60af75680f9a87d63a6c7
atrya-trezer <fedya.mazin@gmail.com>
2023-02-07 19:53:54 +0300

    style

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
 import naming from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
 console.log(naming());
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import readlineSync from 'readline-sync';
 
 export default () => {
-    const name = readlineSync.question('May I have your name? ');
-    return (`Hello, ${name}`);
-}
+  const name = readlineSync.question('May I have your name? ');
+  return (`Hello, ${name}`);
+};
